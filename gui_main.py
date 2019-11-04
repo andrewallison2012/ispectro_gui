07fbe9b5247e5be51d0c5f52e84b343f9c6a618b
ala <andrewallison2012@gmail.com>
2019-11-04 01:01:56 -0600

    working app with auto ranging random data

--- gui_main.py
+++ gui_main.py
@@ -1,6 +1,7 @@
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets, uic
 import numpy as np
+from pyqtgraph.Qt import QtCore, QtGui
 import pyqtgraph as pg
 
 
@@ -14,15 +15,39 @@ class MyWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         Ui_MainWindow.__init__(self)
         self.setupUi(self)
 
+        # sets the plots y max
+        # self.plot1_graphicsView.setYRange(min=0, max=100)
+
+        # sets data for plot
+        self.plot1_graphicsView.plot()
+        self.data = np.empty(100)
+        self.ptr  = 0
+
         # print start after clicking start
         self.start_QPushButton.clicked.connect(self.update_status)
 
+        self.timer = pg.QtCore.QTimer()
+        self.timer.timeout.connect(self.update_data)
+        self.timer.start(50)
 
     # testing
     # print start after clicking start function
     def update_status(self):
         start_text = 'Sweep Started'
         self.bottom_textBrowser.setText(start_text)
+
+    def update_data(self):
+        self.data[self.ptr] = np.random.normal()
+        self.ptr += 1
+        if self.ptr >= self.data.shape[0]:
+            tmp = self.data
+            self.data = np.empty(self.data.shape[0] * 2)
+            self.data[:tmp.shape[0]] = tmp
+        self.plot1_graphicsView.plot().setData(self.data[:self.ptr])
+
+        text = self.sweep_count_spinBox.text()
+        self.bottom_textBrowser.setText(text)
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
