be921be4769db4961356a7f7403d242c24b13260
ala <andrewallison2012@gmail.com>
2019-12-07 19:03:11 -0600

    ino full rev working nice

--- gui_main.py
+++ gui_main.py
@@ -7,8 +7,10 @@ import queue as Queue
 import serial
 from PyQt5.QtCore import QThread, QTimer, QEventLoop, pyqtSignal
 
-port_name ="/dev/ttyACM2"
+port_name ="/dev/ttyACM0"
 baud_rate = 38400
+
+
 
 qtcreator_file  = "ispectro_xml.ui" # Enter file here, this is generated with qt creator or desinger
 Ui_MainWindow, QtBaseClass = uic.loadUiType(qtcreator_file)
@@ -57,35 +59,87 @@ class SerialThread(QtCore.QThread):
         self.transmit = Queue.Queue()
         self.serial_running = True
         self.np_data = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9])
+        self.data_set = np.array([0, 1])
 
     def write_data(self, data_string):
         data = data_string.encode('utf-8')
         self.serial_connection.write(data)
 
-    def angle_calc(self, real, imaginary):
-        pi = np.pi
-        phase = 0
-        theta = 0
+    def data_processing(self, real_unknown, imaginary_unknown, real_cal, imaginary_cal):
+        # function to find theta using x and y from device or calibration x and y from device
+        # just pass in the real and imaginary component
+        def find_angle(real, imaginary):
+            pi = np.pi
+            phase = 0
+            theta = 0
 
-        if real > 0 and imaginary > 0:  # 1st
-            theta = np.arctan(imaginary / real)
-            phase = (theta * 180) / pi
+            # phase calibration with arctangent function accounting for changes in quadrant
+            if real != 0 and imaginary != 0:
+                # positive and positive
+                if real > 0 and imaginary > 0:  # 1st, per page 21 in AD5933 data sheet
+                    theta = np.arctan(imaginary / real)
+                    phase = (theta * 180) / pi
+                # negative and positive
+                if real < 0 and imaginary > 0:  # 2nd, per page 21 in AD5933 data sheet
+                    theta = pi + np.arctan(imaginary / real)
+                    phase = (theta * 180) / pi
+                # negative and negative
+                if real < 0 and imaginary < 0:  # 3rd, per page 21 in AD5933 data sheet
+                    theta = pi + np.arctan(imaginary / real)
+                    phase = (theta * 180) / pi
+                # positive and negative
+                if real > 0 and imaginary < 0:  # 4th, per page 21 in AD5933 data sheet
+                    theta = 2 * (pi) + np.arctan(imaginary / real)
+                    phase = (theta * 180) / pi
 
-        if real < 0 and imaginary > 0:  # 2nd
-            theta = pi + np.arctan(imaginary / real)
-            phase = (theta * 180) / pi
+            # handle arctan function if 'real' aka 'x' component is zero
+            if real == 0:
+                if real == 0 and imaginary > 0:  # 1st, per page 21 in AD5933 data sheet
+                    theta = pi / 2
+                    phase = (theta * 180) / pi
+                    print('1 and 2')
+                if real == 0 and imaginary < 0:  # 4th, per page 21 in AD5933 data sheet
+                    theta = (3 * pi) / 2
+                    phase = (theta * 180) / pi
+                    print('3 and 4')
 
-        if real < 0 and imaginary < 0:  # 3rd
-            theta = pi + np.arctan(imaginary / real)
-            phase = (theta * 180) / pi
+            # handle arctan function if 'imaginary' aka 'y' component is zero
+            if imaginary == 0:
+                if real > 0 and imaginary == 0:  # 1st, per page 21 in AD5933 data sheet
+                    theta = 0
+                    phase = (theta * 180) / pi
+                    print('1 and 4')
+                if real < 0 and imaginary == 0:  # 4th, per page 21 in AD5933 data sheet
+                    theta = pi
+                    phase = (theta * 180) / pi
+                    print('2 and 3')
+            return theta
 
-        if real > 0 and imaginary < 0:  # 4th
-            theta = 2*(pi) + np.arctan(imaginary / real)
-            phase = (theta * 180) / pi
+        # magnitude calculation with gain factor from calibration resistor
+        magnitude_cal = np.sqrt((real_cal ** 2) + (imaginary_cal ** 2))
 
-        print(theta)
-        print(phase)
+        if magnitude_cal != 0:
+            gain_factor = (1 / 1) / magnitude_cal
+        else:
+            gain_factor = (1 / 1) / 1
 
+        if real_unknown != 0:
+            magnitude = np.sqrt((real_unknown ** 2) + (imaginary_unknown ** 2))
+            impedance = 1 / (gain_factor * magnitude)
+        else:
+            magnitude = 1
+            impedance = 1
+
+        theta = find_angle(real_unknown, imaginary_unknown)
+
+        # adjusting vector projection to adjust for phase angle and magnitude
+        z_real = np.abs(impedance) * np.cos(theta)
+        z_imaginary = np.abs(impedance) * np.sin(theta)
+
+        string_to_print = f'calibrated impedace magnitude: {impedance}\ntheta: {theta}\nz_real: {z_real}\nz_imaginary: {z_imaginary}'
+        self.data_set = np.vstack((self.data_set, np.array([z_real,z_imaginary])))
+        return self.data_set
+        print(string_to_print)
 
     def run(self):
         self.np_data = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9])
@@ -107,7 +161,7 @@ class SerialThread(QtCore.QThread):
             if np.array_equal(last_line, line) == False:
                 self.np_data = np.vstack((self.np_data, line))
                 print(line)
-                self.angle_calc(line[5], line[6])
+                self.data_processing(line[1], line[2], line[3], line[4])
 
         if self.serial_connection:
             self.serial_connection.close()
@@ -120,22 +174,26 @@ class MyWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         Ui_MainWindow.__init__(self)
 
         self.setupUi(self)
-
         self.update_status("iSpectro Loaded -- Welcome")
         self.plot1_graphicsView.setDownsampling(mode='peak')
         self.plot1_graphicsView.setClipToView(True)
 
-        plot = self.plot1_graphicsView
+
+        self.plot1_graphicsView.showGrid(x=True, y=True)
+        self.plot1_graphicsView.setTitle(title="Impedance Z")
+        self.plot1_graphicsView.setLabel('left',text= 'Ohms (imaginary)')
+        self.plot1_graphicsView.setLabel('bottom',text='Ohms (real)')
+
 
         self.start_QPushButton.clicked.connect(self.start_sweep)
         self.stop_QPushButton.clicked.connect(self.stop_sweep)
+        self.connect_pushButton.clicked.connect(self.connect)
 
         self.timer = pg.QtCore.QTimer()
         self.timer.start(250)
         self.timer.timeout.connect(self.update)
 
-        self.serial_thread = SerialThread(port_name, baud_rate)
-        self.serial_thread.start()
+
 
         # self.graph_thread = SerialGraphThread(connection_object=self.serial_thread.np_data, plot1_graphicsView=plot)
         # self.graph_thread.start()
@@ -151,6 +209,11 @@ class MyWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def stop_sweep(self):
         self.serial_thread.write_data('<STOP>')
         self.update_status("Sweep Aborted")
+        self.serial_thread.serial_running = False
+
+    def connect(self):
+        self.serial_thread = SerialThread(port_name, baud_rate)
+        self.serial_thread.start()
 
     def update_status(self, text):
         self.bottom_textBrowser.append(dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S -- ') +text)
@@ -167,9 +230,9 @@ class MyWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         # pi = 3.14
 
         # self.plot1_graphicsView.plot().setData(x=self.arduino_connection.np_data[1:,0],y=self.arduino_connection.np_data[1:,7], pen=None, symbol='x')
-        for i in range(np.alen(self.serial_thread.np_data[1:,])):
-            spots3.append({'pos': (self.serial_thread.np_data[-1:,5], self.serial_thread.np_data[-1,6]),
-                           'brush': pg.intColor(i, 120)})
+        for i in range(np.alen(self.serial_thread.data_set[1:,])):
+            spots3.append({'pos': (self.serial_thread.data_set[-1:,0], self.serial_thread.data_set[-1,1]),
+                           'brush': pg.intColor(i, 120, alpha = 20), 'pen': pg.mkPen(None)})
 
 
         view = self.plot1_graphicsView
